468fd300930b7a875216ee948d6e490d15d66f12
John Benninghoff <jbenninghoff@MBpro.local>
2013-08-12 12:20:10 -0700

    Simplified summIOzone.sh, now runnable via clush

--- pre-install/summIOzone.sh
+++ pre-install/summIOzone.sh
@@ -4,118 +4,77 @@
 # iozone results must be concatenated to a single log file like this:
 # cat sd*iozone.log > tmp.log; ./summIOzone.sh tmp.log
 
-gawk '
+cat sd*iozone.log | gawk '
    BEGIN {
-     min=600000
+     swmin=6000000; srmin=swmin; rrmin=swmin; rwmin=swmin
    }
 
-# Match begining of IOzone output line
+# Match begining of IOzone output line and capture data fields
    /         4194304    1024/ {
+     # err chk if argc < 8
      count++
-     total += $3
-     vals[count] = $3
-     if ($3 < min) min = $3
-     if ($3 > max) max = $3
+     swtotal += $3
+     srtotal += $5
+     rrtotal += $7
+     rwtotal += $8
+     swvals[count] = $3
+     srvals[count] = $5
+     rrvals[count] = $7
+     rwvals[count] = $8
+     if ($3 < swmin) swmin = $3; if ($3 > swmax) swmax = $3
+     if ($5 < srmin) srmin = $5; if ($5 > srmax) srmax = $5
+     if ($7 < rrmin) rrmin = $7; if ($7 > rrmax) rrmax = $7
+     if ($8 < rwmin) rwmin = $8; if ($8 > rwmax) rwmax = $8
    }
 
    END {
      print "IOzone Sequential Write Summary(KB/sec)"
-     avg = total/count
+     swavg = swtotal/count
      printf "%-7s %6d\n", "count:", count
-     printf "%-7s %6d\n", "min:", min
-     printf "%-7s %6d\n", "max:", max
-     printf "%-7s %6d\n", "mean:", avg
-     for (val in vals) {
-       svals += (vals[val] - avg) ** 2
+     printf "%-7s %6d\n", "min:", swmin
+     printf "%-7s %6d\n", "max:", swmax
+     printf "%-7s %6d\n", "mean:", swavg
+     for (val in swvals) {
+       svals += (swvals[val] - swavg) ** 2
      }
 #     print "stdev: ", sqrt(svals/count)
-     printf "CV: %00.1f%%\n", 100*(sqrt(svals/count) / avg)
-   }
-' $1
+     printf "CV: %00.1f%%\n", 100*(sqrt(svals/count) / swavg)
 
-gawk '
-   BEGIN {
-     min=600000
-   }
-
-# Match begining of IOzone output line
-   /         4194304    1024/ {
-     count++
-     vals[count] = $5
-     total += $5
-     if ($5 < min) min = $5
-     if ($5 > max) max = $5
-   }
-
-   END {
-     avg = total/count
      print "IOzone Sequential Read Summary(KB/sec)"
+     sravg = srtotal/count
      printf "%-7s %6d\n", "count:", count
-     printf "%-7s %6d\n", "min:", min
-     printf "%-7s %6d\n", "max:", max
-     printf "%-7s %6d\n", "mean:", avg
-     for (val in vals) {
-       svals += (vals[val] - avg) ** 2
+     printf "%-7s %6d\n", "min:", srmin
+     printf "%-7s %6d\n", "max:", srmax
+     printf "%-7s %6d\n", "mean:", sravg
+     for (val in srvals) {
+       svals += (srvals[val] - sravg) ** 2
      }
 #     print "stdev: ", sqrt(svals/count)
-     printf "CV: %00.1f%%\n", 100*(sqrt(svals/count) / avg)
-   }
-' $1
+     printf "CV: %00.1f%%\n", 100*(sqrt(svals/count) / sravg)
 
-gawk '
-   BEGIN {
-     min=600000
-   }
-
-# Match begining of IOzone output line
-   /         4194304    1024/ {
-     count++
-     vals[count] = $7
-     total += $7
-     if ($7 < min) min = $7
-     if ($7 > max) max = $7
-   }
-
-   END {
-     avg = total/count
-     print "IOzone Random Read Summary(KB/sec)"
-     printf "%-7s %6d\n", "count:", count
-     printf "%-7s %6d\n", "min:", min
-     printf "%-7s %6d\n", "max:", max
-     printf "%-7s %6d\n", "mean:", avg
-     for (val in vals) {
-       svals += (vals[val] - avg) ** 2
-     }
-#     print "stdev: ", sqrt(svals/count)
-     printf "CV: %00.1f%%\n", 100*(sqrt(svals/count) / avg)
-   }
-' $1
-
-gawk '
-   BEGIN {
-     min=600000
-   }
-
-# Match begining of IOzone output line
-   /         4194304    1024/ {
-     count++
-     vals[count] = $8
-     total += $8
-     if ($8 < min) min = $8
-     if ($8 > max) max = $8
-   }
-
-   END {
-     avg = total/count
      print "IOzone Random Write Summary(KB/sec)"
+     rwavg = rwtotal/count
      printf "%-7s %6d\n", "count:", count
-     printf "%-7s %6d\n", "min:", min
-     printf "%-7s %6d\n", "max:", max
-     printf "%-7s %6d\n", "mean:", avg
-     for (val in vals) {
-       svals += (vals[val] - avg) ** 2
+     printf "%-7s %6d\n", "min:", rwmin
+     printf "%-7s %6d\n", "max:", rwmax
+     printf "%-7s %6d\n", "mean:", rwavg
+     for (val in rwvals) {
+       svals += (rwvals[val] - rwavg) ** 2
      }
 #     print "stdev: ", sqrt(svals/count)
-     printf "CV: %00.1f%%\n", 100*(sqrt(svals/count) / avg)
+     printf "CV: %00.1f%%\n", 100*(sqrt(svals/count) / rwavg)
+
+     print "IOzone Random Read Summary(KB/sec)"
+     rravg = rrtotal/count
+     printf "%-7s %6d\n", "count:", count
+     printf "%-7s %6d\n", "min:", rrmin
+     printf "%-7s %6d\n", "max:", rrmax
+     printf "%-7s %6d\n", "mean:", rravg
+     for (val in rrvals) {
+       svals += (rrvals[val] - rravg) ** 2
+     }
+#     print "stdev: ", sqrt(svals/count)
+     printf "CV: %00.1f%%\n", 100*(sqrt(svals/count) / rravg)
    }
-' $1
+'
+
