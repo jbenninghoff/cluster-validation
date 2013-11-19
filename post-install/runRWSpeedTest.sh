a7de09f975fe16f4245edc0287a89650bfe20d70
John Benninghoff <john.benninghoff@gmail.com>
2013-11-19 15:44:15 -0800

    More tweaks

--- post-install/runRWSpeedTest.sh
+++ post-install/runRWSpeedTest.sh
@@ -5,7 +5,7 @@
 # Simple script to run the MapR RWSpeedTest on a single node.
 #
 # RWSpeedTest is NOT like DFSIO ... it is not a MapReduce job.
-# Thus, the client driver is more likely to be the performance bottleneck.
+# So to run on all nodes in cluster you need to use clush
 
 MAPR_HOME=${MAPR_HOME:-/opt/mapr}
 localvol=localvol-$(hostname -s)
@@ -17,12 +17,18 @@ hadoop mfs -setcompression off /localvol
 #find jars, there should only be one of these jars ... let's hope :)
 MFS_TEST_JAR=$(find $MAPR_HOME/lib -name maprfs-diagnostic-tools-\*.jar)
 
+# Find the total number of cores and use half of them
 ncpu=$(grep -c ^processor /proc/cpuinfo)
 ((ncpu=ncpu/2))
 
+# Find the available MapR disk space on this node
 fsize=$(/opt/mapr/server/mrconfig sp list | awk '/totalfree/{print $9}')
-((fsize=(fsize/100)/ncpu)) #Should be big enough to exceed MFS cache
+# Use 100th of available space divided by the number of cores that wil be used
+((fsize=(fsize/100)/ncpu)) #Check if big enough to exceed MFS cache
 
+#usage: RWSpeedTest filename [-]megabytes uri
+
+# A simple single core (1 process) test to verify node if needed
 #hadoop jar $MFS_TEST_JAR com.mapr.fs.RWSpeedTest /$localvol/RWTestSingleTest $fsize maprfs:/// ; exit
 
 #run RWSpeedTest writes
--- post-install/runTeraSort.sh
+++ post-install/runTeraSort.sh
@@ -2,32 +2,37 @@
 # jbenninghoff@maprtech.com 2013-Mar-8 vi: set ai et sw=3 tabstop=3:
 
 nodes=$(maprcli node list -columns hostname,cpus,ttReduceSlots | awk '/^[1-9]/{if ($2>1) count++};END{print count}')
-((rtasks=nodes*2)) # 2 reduce tasks per node, reduce tasks per node limited by available RAM
+((rtasks=nodes*2)) # Start with 2 reduce tasks per node, reduce tasks per node limited by available RAM
 
-# TeraSort
+# TeraSort baseline execution, start with this before experimenting with any options in the block comment
 hadoop fs -rmr /benchmarks/tera/run1
 hadoop jar /opt/mapr/hadoop/hadoop-0.20.2/hadoop-0.20.2-dev-examples.jar terasort \
 -Dmapred.reduce.tasks=$rtasks \
+-Dmapred.reduce.child.java.opts="-Xmx3000m" \
+-Dmapred.job.shuffle.input.buffer.percent=0.7 \
 /benchmarks/tera/in /benchmarks/tera/run1
 
+# Capture the job history log
 logname=terasort-run1-$(date -Imin|cut -c-16).log
 hadoop job -history /benchmarks/tera/run1 > $logname  # capture the run log
-head -22 $logname  # show the top of the log with elapsed time, etc
+head -32 $logname  # show the top of the log with elapsed time, etc
 
 # To validate TeraSort output, uncomment below and change output folder
 # hadoop jar /opt/mapr/hadoop/hadoop-0.20.2/hadoop-0.20.2-dev-examples.jar teravalidate /benchmarks/tera/run1 /benchmarks/tera/run1validate
 
 : << '--BLOCK-COMMENT--'
+# Experimental options below, use if you know what you are doing
 -Dmapred.map.tasks.speculative.execution=false \
 -Dmapred.reduce.tasks.speculative.execution=false \
+# Default io.sort.mb is 380MB which is sufficient for default MFS ChunkSize (256MB)
 -Dio.sort.mb=380 \
+# Default map and reduce heap sizes are auto-tuned, should work out of the box per Srivas
 -Dmapred.map.child.java.opts="-Xmx1000m" \
 -Dmapred.reduce.child.java.opts="-Xmx3000m" \
-#1TB requires 334 reduce tasks using 3GB heap, 250 reduce tasks using 4GB heap
-# Experimental options below, use if you know what you are doing
+#1TB requires 334 reduce tasks using 3GB heap, 250 reduce tasks using 4GB heap, to fit all 1TB in RAM
 # slowstart setting controls overlap of reduce tasks with map tasks
 -Dmapred.reduce.slowstart.completed.maps=0 \
-# Parallel copies has impact, match map task count?
+# Parallel copies has mixed results
 -Dmapred.reduce.parallel.copies=4 \
 # Disable logging for final peak performance run ??
 -Dmapreduce.map.log.level=NONE \
@@ -35,19 +40,25 @@ head -22 $logname  # show the top of the log with elapsed time, etc
 # Disable compression for sortbenchmark.org rules
 -Dmapreduce.maprfs.use.compression=false \
 # options used in world record terasort run on large cluster:
-# buffer.percent triggers BUG in MapR 2.1x
--Dmapred.job.reduce.input.buffer.percent=1.0 \
 -Dmapred.maxthreads.generate.mapoutput=6 \
 -Dmapred.maxthreads.partition.closer=6 \
+# Apache docs on important reduce task settings:
+# http://hadoop.apache.org/docs/r1.2.1/mapred_tutorial.html#Shuffle%2FReduce+Parameters
+# percentage of heap to store map outputs during the shuffle.
 -Dmapred.job.shuffle.input.buffer.percent=0.70 \
 -Dmapred.job.shuffle.merge.percent=1.0 \
 
+#Bundle of reduce options to try:
+-Dmapred.reduce.child.java.opts="-Xmx3000m" \
+-Dmapred.job.shuffle.input.buffer.percent=0.7 \
+-Dmapred.job.shuffle.merge.percent=1.0 \
+
 # TeraSort options worth experimenting with below:
+# buffer.percent triggers BUG in MapR 2.1x
+-Dmapred.job.reduce.input.buffer.percent=1.0 \
 -Dmapred.job.shuffle.input.buffer.percent=0.8 \
 -Dmapred.inmem.merge.threshold=0 \
 -Dmapred.job.shuffle.merge.percent=1.0 \
--Dmapred.job.reduce.input.buffer.percent=1.0 \
--Dmapred.reduce.parallel.copies=2 \
 -Dio.sort.factor=20 \
 -Dmapreduce.jobtracker.heartbeat.interval.min=1 \
 -Dmapreduce.tasktracker.prefetch.maptasks=0.0 \
@@ -58,6 +69,4 @@ head -22 $logname  # show the top of the log with elapsed time, etc
 
 #tasktracker.http.threads=70
 # terasort param ignored in new terasort package:  -Dmapred.map.tasks=$nf 
-#hadoop fs -ls /benchmarks/tera/run1/_partition.lst  #file in java io error, volume issue?
-
 --BLOCK-COMMENT--
