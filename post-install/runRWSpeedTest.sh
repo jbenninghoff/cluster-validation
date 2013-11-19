7fa0ce66b085131db63f1fe4868a579ed25a71a4
John Benninghoff <jbenninghoff@maprtech.com>
2013-11-18 18:02:42 -0800

    Fixed RWspeedTest, simplified runTeraSort

--- post-install/runRWSpeedTest.sh
+++ post-install/runRWSpeedTest.sh
@@ -8,9 +8,10 @@
 # Thus, the client driver is more likely to be the performance bottleneck.
 
 MAPR_HOME=${MAPR_HOME:-/opt/mapr}
+localvol=localvol-$(hostname -s)
 
 # Make local volume
-maprcli volume create -name localvol -path /localvol -replication 1 -localvolumehost $(<$MAPR_HOME/hostname)
+maprcli volume create -name $localvol -path /$localvol -replication 1 -localvolumehost $(<$MAPR_HOME/hostname)
 hadoop mfs -setcompression off /localvol
 
 #find jars, there should only be one of these jars ... let's hope :)
@@ -20,17 +21,19 @@ ncpu=$(grep -c ^processor /proc/cpuinfo)
 ((ncpu=ncpu/2))
 
 fsize=$(/opt/mapr/server/mrconfig sp list | awk '/totalfree/{print $9}')
-((fsize=(fsize/100)/ncpu))
+((fsize=(fsize/100)/ncpu)) #Should be big enough to exceed MFS cache
+
+#hadoop jar $MFS_TEST_JAR com.mapr.fs.RWSpeedTest /$localvol/RWTestSingleTest $fsize maprfs:/// ; exit
 
 #run RWSpeedTest writes
 for i in $(seq 1 $ncpu); do
-   hadoop jar $MFS_TEST_JAR com.mapr.fs.RWSpeedTest /localvol/RWTest${i} $fsize maprfs:/// &
+   hadoop jar $MFS_TEST_JAR com.mapr.fs.RWSpeedTest /$localvol/RWTest${i} $fsize maprfs:/// &
 done
 wait
 
 #run RWSpeedTest reads
 for i in $(seq 1 $ncpu); do
-   hadoop jar $MFS_TEST_JAR com.mapr.fs.RWSpeedTest /localvol/RWTest${i} -$fsize maprfs:/// &
+   hadoop jar $MFS_TEST_JAR com.mapr.fs.RWSpeedTest /$localvol/RWTest${i} -$fsize maprfs:/// &
 done
 wait
 
