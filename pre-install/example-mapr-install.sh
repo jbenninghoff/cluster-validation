bf5d88bb537ca7891b5593c21cc38c4f242dd64f
John Benninghoff <john.benninghoff@gmail.com>
2014-03-13 12:43:56 -0500

    updated example-mapr-install.sh

--- pre-install/example-mapr-install.sh
+++ pre-install/example-mapr-install.sh
@@ -8,8 +8,12 @@ cat - << 'EOF'
 # Assumes MapR yum repo or repo mirror has been configured for all nodes and that MapR will run as mapr user
 # Assumes jt, zkcldb and all group have been defined for clush
 EOF
+[ $(id -u) -ne 0 ] && { echo; echo You must be root with passwordless ssh to all nodes to run this script; exit 1; }
+
 # Identify and format the data disks for MapR, destroys all data on all disks listed in /tmp/disk.list on all nodes
 clush -Ba "lsblk -id | grep -o ^sd. | grep -v ^sda |sort|sed 's,^,/dev/,' | tee /tmp/disk.list; wc /tmp/disk.list"
+echo Scrutinize the disk list above.  All disks will be formatted for MapR FS, destroying all existing data on the disks
+echo Once the disk list is approved, edit this script and remove or comment the exit statement below
 exit
 
 # Install all servers with minimal rpms to provide storage and compute plus NFS
@@ -23,11 +27,11 @@ clush -B -g zkcldb 'yum -y install mapr-zookeeper' #3 Zookeeper nodes, fileserve
 
 # Service Layout option #2 ====================
 # Admin services on dedicated nodes, uncomment the line below
-#clush -B -w host1,host2,host3,host4,host5,host6,host7,host8 'yum -y erase mapr-tasktracker'
+#clush -B -g jt,zkcldb 'yum -y erase mapr-tasktracker'
 
 # Configure ALL nodes with CLDB and Zookeeper info (-N does not like spaces in the name)
 clush -B -a "/opt/mapr/server/configure.sh -N MyCluster -Z $(nodeset -S, -e @zkcldb) -C $(nodeset -S, -e @zkcldb) -u mapr -g mapr"
-[ $? -ne 0 ] && {echo configure.sh failed, check screen for errors; exit 1}
+[ $? -ne 0 ] && { echo configure.sh failed, check screen for errors; exit 2; }
 
 # Identify and format the data disks for MapR
 clush -B -a "lsblk -id | grep -o ^sd. | grep -v ^sda |sort|sed 's,^,/dev/,' | tee /tmp/disk.list; wc /tmp/disk.list"
@@ -35,14 +39,17 @@ clush -B -a '/opt/mapr/server/disksetup -F /tmp/disk.list'
 #clush -B -a '/opt/mapr/server/disksetup -W $(cat /tmp/disk.list | wc -l) -F /tmp/disk.list' #Fast but less resilient storage
 
 # Check for correct java version and set JAVA_HOME
-clush -ab -o -qtt 'sudo sed -i "s,^#export JAVA_HOME=,export JAVA_HOME=/usr/java/jdk1.7.0_51," /opt/mapr/conf/env.sh'
+clush -ab 'sed -i "s,^#export JAVA_HOME=,export JAVA_HOME=/usr/java/jdk1.7.0_51," /opt/mapr/conf/env.sh'
 
 clush -B -g zkcldb service mapr-zookeeper start; sleep 10
-ssh host3 service mapr-warden start  # Start 1 CLDB and webserver
+ssh $(nodeset -I0 -e @zkcldb) service mapr-warden start  # Start 1 CLDB and webserver
 echo Wait at least 90 seconds for system to initialize; sleep 90
 
-ssh host3 maprcli acl edit -type cluster -user mapr:fc
+ssh $(nodeset -I0 -e @zkcldb) maprcli node cldbmaster && ssh $(nodeset -I0 -e @zkcldb) maprcli acl edit -type cluster -user mapr:fc
+[ $? -ne 0 ] && { echo CLDB did not startup, check status and logs on $(nodeset -I0 -e @zkcldb); exit 3; }
+
 echo With a web browser, open this URL to continue with license installation:
-echo 'https://host3:8443/'
+echo "https://$(nodeset -I0 -e @zkcldb):8443/"
 echo
 echo Start mapr-warden on all remaining servers once the license is applied
+echo clush -Ba service mapr-warden start
