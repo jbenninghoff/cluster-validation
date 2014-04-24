a406c6c0d4409cd764d8d48ad66e10b055d0a348
John Benninghoff <john.benninghoff@gmail.com>
2014-04-23 22:31:24 -0700

    sudo change to cluster-audit.sh

--- post-install/runTeraSort.sh
+++ post-install/runTeraSort.sh
@@ -9,16 +9,20 @@ echo rtasks=$rtasks
 hadoop fs -rmr /benchmarks/tera/run1
 hadoop jar /opt/mapr/hadoop/hadoop-0.20.2/hadoop-0.20.2-dev-examples.jar terasort \
 -Dmapred.reduce.tasks=$rtasks \
--Dmapred.reduce.child.java.opts=" -Xmx3000m" \
+-Dmapred.reduce.child.java.opts="-Xmx3000m" \
+-Dmapred.reduce.tasks.speculative.execution=false \
+-Dmapred.map.tasks.speculative.execution=false \
 /benchmarks/tera/in /benchmarks/tera/run1
 
 # Capture the job history log
 logname=terasort-run1-$(date -Imin|cut -c-16).log
 hadoop job -history /benchmarks/tera/run1 > $logname  # capture the run log
+cat $0 >> $logname
 head -32 $logname  # show the top of the log with elapsed time, etc
 
 # To validate TeraSort output, uncomment below and change output folder
 # hadoop jar /opt/mapr/hadoop/hadoop-0.20.2/hadoop-0.20.2-dev-examples.jar teravalidate /benchmarks/tera/run1 /benchmarks/tera/run1validate
 
 #-Dmapred.reduce.slowstart.completed.maps=0.25 \
+#-Dmapred.map.child.java.opts="-Xmx880m" \
 
--- pre-install/example-mapr-install.sh
+++ pre-install/example-mapr-install.sh
@@ -6,7 +6,7 @@ cat - << 'EOF'
 # Assumes all nodes have been audited with cluster-audit.sh and all issues fixed
 # Assumes all nodes have met subsystem performance expectations as measured by memory-test.sh, network-test.sh and disk-test.sh
 # Assumes MapR yum repo or repo mirror has been configured for all nodes and that MapR will run as mapr user
-# Assumes jt, zkcldb and all group have been defined for clush
+# Assumes jt, zkcldb and all group have been defined for clush in /etc/clustershell/groups
 EOF
 [ $(id -u) -ne 0 ] && { echo; echo You must be root with passwordless ssh to all nodes to run this script; exit 1; }
 
@@ -20,36 +20,37 @@ exit
 clush -B -a 'yum -y install mapr-fileserver mapr-nfs mapr-tasktracker'
 
 # Service Layout option #1 ====================
-# Admin services layered over various data nodes
+# Admin services layered over data nodes defined in jt and zkcldb groups
 clush -B -g jt 'yum -y install mapr-jobtracker mapr-webserver mapr-metrics' # At least 2 JobTracker nodes
 clush -B -g zkcldb 'yum -y install mapr-cldb mapr-webserver' # 3 CLDB nodes for HA, 1 does writes, all 3 do reads
-clush -B -g zkcldb 'yum -y install mapr-zookeeper' #3 Zookeeper nodes, fileserver and nfs could be erased
+clush -B -g zkcldb 'yum -y install mapr-zookeeper' #3 Zookeeper nodes, fileserver, tt and nfs could be erased
 
 # Service Layout option #2 ====================
 # Admin services on dedicated nodes, uncomment the line below
 #clush -B -g jt,zkcldb 'yum -y erase mapr-tasktracker'
 
-# Configure ALL nodes with CLDB and Zookeeper info (-N does not like spaces in the name)
+# Configure ALL nodes with the CLDB and Zookeeper info (-N does not like spaces in the name)
 clush -B -a "/opt/mapr/server/configure.sh -N MyCluster -Z $(nodeset -S, -e @zkcldb) -C $(nodeset -S, -e @zkcldb) -u mapr -g mapr"
 [ $? -ne 0 ] && { echo configure.sh failed, check screen for errors; exit 2; }
 
 # Identify and format the data disks for MapR
-clush -B -a "lsblk -id | grep -o ^sd. | grep -v ^sda |sort|sed 's,^,/dev/,' | tee /tmp/disk.list; wc /tmp/disk.list"
+#clush -B -a "lsblk -id | grep -o ^sd. | grep -v ^sda |sort|sed 's,^,/dev/,' | tee /tmp/disk.list; wc /tmp/disk.list"
 clush -B -a '/opt/mapr/server/disksetup -F /tmp/disk.list'
 #clush -B -a '/opt/mapr/server/disksetup -W $(cat /tmp/disk.list | wc -l) -F /tmp/disk.list' #Fast but less resilient storage
 
 # Check for correct java version and set JAVA_HOME
 clush -ab 'sed -i "s,^#export JAVA_HOME=,export JAVA_HOME=/usr/java/jdk1.7.0_51," /opt/mapr/conf/env.sh'
 
+node1=$(nodeset -I0 -e @zkcldb) #first node in zkcldb group
 clush -B -g zkcldb service mapr-zookeeper start; sleep 10
-ssh $(nodeset -I0 -e @zkcldb) service mapr-warden start  # Start 1 CLDB and webserver
+ssh $node1 service mapr-warden start  # Start 1 CLDB and webserver on first node
 echo Wait at least 90 seconds for system to initialize; sleep 90
 
-ssh $(nodeset -I0 -e @zkcldb) maprcli node cldbmaster && ssh $(nodeset -I0 -e @zkcldb) maprcli acl edit -type cluster -user mapr:fc
-[ $? -ne 0 ] && { echo CLDB did not startup, check status and logs on $(nodeset -I0 -e @zkcldb); exit 3; }
+ssh $node1 maprcli node cldbmaster && ssh $node1 maprcli acl edit -type cluster -user mapr:fc
+[ $? -ne 0 ] && { echo CLDB did not startup, check status and logs on $node1; exit 3; }
 
 echo With a web browser, open this URL to continue with license installation:
-echo "https://$(nodeset -I0 -e @zkcldb):8443/"
+echo "https://$node1:8443/"
 echo
 echo Start mapr-warden on all remaining servers once the license is applied
 echo clush -Ba service mapr-warden start
