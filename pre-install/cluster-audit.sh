2815a2bd381dedf6b3278a731c82713dd2a3e887
John Benninghoff <john.benninghoff@gmail.com>
2014-04-23 22:31:24 -0700

    sudo change to cluster-audit.sh

--- pre-install/cluster-audit.sh
+++ pre-install/cluster-audit.sh
@@ -9,19 +9,22 @@ sep='====================================================================='
 D=$(dirname "$0")
 abspath=$(cd "$D" 2>/dev/null && pwd || echo "$D")
 eval enpath=$(echo /sys/kernel/mm/transparent_hugepage/enabled) #needs improvement
-distro=$(cat /etc/*release | grep -m1 -i -o -e ubuntu -e redhat -e 'red hat' -e centos)
+distro=$(cat /etc/*release | grep -m1 -i -o -e ubuntu -e redhat -e 'red hat' -e centos) || distro=centos
 shopt -s nocasematch
 
 #parg="-a -x rhel11,rhel16"
 parg="-g sen -B"
 parg="-aB"
+parg="-B -w local"
 parg2='-g sen -B -o -qtt'
 parg2='-aB -o -qtt'
+parg2='-B -w local -o -qtt'
 
 echo ==================== Hardware audits ================================
 date; echo $sep
 # probe for system info ###############
 clush $parg2 "sudo dmidecode |grep -A2 '^System Information'"; echo $sep
+#clush $parg2 'sudo echo $PATH'; exit
 clush $parg2 "sudo dmidecode | grep -A3 '^BIOS I'"; echo $sep
 # probe for mem/dimm info ###############
 clush $parg "cat /proc/meminfo | grep -i ^memt | uniq"; echo $sep
@@ -35,12 +38,12 @@ clush $parg "lscpu | grep -v -e op-mode -e ^Vendor -e family -e Model: -e Steppi
 # probe for nic info ###############
 #clush $parg "ifconfig | grep -o ^eth.| xargs -l sudo /usr/sbin/ethtool | grep -e ^Settings -e Speed" 
 #clush $parg "ifconfig | awk '/^[^ ]/ && \$1 !~ /lo/{print \$1}' | xargs -l sudo /usr/sbin/ethtool | grep -e ^Settings -e Speed" 
-clush $parg "lspci | grep -i ether"
+clush $parg "/sbin/lspci | grep -i ether"
 clush $parg2 "/sbin/ip link show | sed '/ lo: /,+1d' | awk '/UP/{sub(\":\",\"\",\$2);print \$2}' | xargs -l sudo ethtool | grep -e ^Settings -e Speed"
 #clush $parg "echo -n 'Nic Speed: '; /sbin/ip link show | sed '/ lo: /,+1d' | awk '/UP/{sub(\":\",\"\",\$2);print \$2}' | xargs -l -I % cat /sys/class/net/%/speed"
 echo $sep
 # probe for disk info ###############
-clush $parg "echo 'Storage Controller: '; lspci | grep -i -e raid -e storage -e lsi"; echo $sep
+clush $parg "echo 'Storage Controller: '; /sbin/lspci | grep -i -e raid -e storage -e lsi"; echo $sep
 clush $parg "dmesg | grep -i raid | grep -i scsi"; echo $sep
 case $distro in
    ubuntu)
@@ -49,7 +52,7 @@ case $distro in
    redhat|centos|red*)
 	clush $parg "lsblk -id | awk '{print \$1,\$4}'|sort | nl"; echo $sep
    ;;
-   *) echo Unknown Linux distro!; exit ;;
+   *) echo Unknown Linux distro! $distro; exit ;;
 esac
 clush $parg "df -hT | cut -c23-28,39- | grep -e '  *' | grep -v -e /dev"; echo $sep
 #clush $parg "echo 'Storage Drive(s): '; fdisk -l 2>/dev/null | grep '^Disk /dev/.*: ' | sort | grep mapper"
@@ -60,7 +63,7 @@ echo $sep
 clush $parg "cat /etc/*release | uniq"; echo $sep
 clush $parg "uname -srvm | fmt"; echo $sep
 clush $parg date; echo $sep
-clush $parg sysctl vm.swappiness net.ipv4.tcp_retries2 vm.overcommit_memory
+clush $parg /sbin/sysctl vm.swappiness net.ipv4.tcp_retries2 vm.overcommit_memory
 echo -e "/etc/sysctl.conf values should be:\nvm.swappiness = 0\nnet.ipv4.tcp_retries2 = 2\nvm.overcommit_memory = 0"
 echo $sep
 
@@ -69,9 +72,9 @@ case $distro in
       # Ubuntu SElinux tools not so good.
       clush $parg2 "sudo apparmor_status | sed 's/([0-9]*)//'"; echo $sep
       clush $parg "echo -n 'SElinux status: '; ([ -d /etc/selinux -a -f /etc/selinux/config ] && grep ^SELINUX= /etc/selinux/config) || echo Disabled"; echo $sep
-      clush $parg "echo 'Firewall status: '; service ufw status | head -10"; echo $sep
+      clush $parg "echo 'Firewall status: '; /sbin/service ufw status | head -10"; echo $sep
       clush $parg2 "echo 'IPtables status: '; sudo iptables -L | head -10"; echo $sep
-      clush $parg 'echo "NTP status "; service ntp status'; echo $sep
+      clush $parg 'echo "NTP status "; /sbin/service ntp status'; echo $sep
       clush $parg "echo 'NFS packages installed '; dpkg -l '*nfs*' | grep ^i"; echo $sep
    ;;
    redhat|centos|red*)
@@ -79,14 +82,14 @@ case $distro in
       clush $parg "echo -n 'SElinux status: '; grep ^SELINUX= /etc/selinux/config" ; echo $sep
       clush $parg "/sbin/chkconfig --list iptables" ; echo $sep
       #clush $parg "/sbin/service iptables status | grep -m 3 -e ^Table -e ^Chain" 
-      clush $parg "service iptables status | head -10"; echo $sep
+      clush $parg "/sbin/service iptables status | head -10"; echo $sep
       #clush $parg "echo -n 'Frequency Governor: '; for dev in /sys/devices/system/cpu/cpu[0-9]*; do cat \$dev/cpufreq/scaling_governor; done | uniq -c" 
-      clush $parg "echo -n 'CPUspeed Service: '; service cpuspeed status" 
+      clush $parg "echo -n 'CPUspeed Service: '; /sbin/service cpuspeed status" 
       clush $parg "echo -n 'CPUspeed Service: '; /sbin/chkconfig --list cpuspeed"; echo $sep
       clush $parg 'echo "NFS packages installed "; rpm -qa | grep -i nfs |sort' ; echo $sep
-      clush $parg 'echo Missing RPMs: ; for each in make patch redhat-lsb irqbalance syslinux hdparm sdparm dmidecode nc rpcbind; do rpm -q $each | grep "is not installed"; done' ; echo $sep
+      clush $parg 'echo Missing RPMs: ; for each in make patch redhat-lsb irqbalance syslinux hdparm sdparm dmidecode nc rpcbind nfs-utils dstat redhat-lsb-core git gcc openjdk-devel; do rpm -q $each | grep "is not installed"; done' ; echo $sep
    ;;
-   *) echo Unknown Linux distro!; exit ;;
+   *) echo Unknown Linux distro! $distro; exit ;;
 esac
 shopt -u nocasematch
 
@@ -98,6 +101,6 @@ clush $parg 'java -XX:+PrintFlagsFinal -version |& grep MaxHeapSize'; echo $sep
 echo Hostname lookup
 clush $parg 'hostname -I'; echo $sep
 clush $parg "ls -d /opt/mapr/* | head" ; echo $sep
-clush $parg2 'echo -n "Open file limit(should be >32K): "; su - mapr -c "ulimit -n"' ; echo $sep
+clush $parg2 'echo -n "Open file limit for mapr(should be >32K): "; sudo su - mapr -c "ulimit -n"' ; echo $sep
 clush $parg2 'echo "mapr login for Hadoop "; getent passwd mapr && { sudo echo ~mapr/.ssh; sudo ls ~mapr/.ssh; }'; echo $sep
 clush $parg2 'echo "Root login "; getent passwd root && { sudo echo ~root/.ssh; sudo ls ~root/.ssh; }'; echo $sep
