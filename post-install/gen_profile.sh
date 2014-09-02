eb1ecd75ac7295fe81f68e93706232eb2df218d6
Dmitry Gomerman <dgomerman@maprtech.com>
2014-09-02 12:51:32 -0700

    Use sudo only when not already root. This prevents issues with clush when "Defaults requiretty" is set in /etc/sudoers.

--- post-install/gen_profile.sh
+++ post-install/gen_profile.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 # dgomerman@maprtech.com 2014-Mar-25
 OUTPUT="/tmp/node_profile.sh"
+SUDO=""
+[ $(id -u) -ne 0 ] && SUDO=sudo
+
 # OS
 distro=$(cat /etc/*release | grep -m1 -i -o -e ubuntu -e redhat -e centos)
 if [ ! -n "$distro" ] ; then
     distro="unknown"
 fi
 # Manufacturer
-manufacturer=$(sudo dmidecode |grep -A2 '^System Information' |grep -i "Manufacturer" |sed -e 's/^.*: //')
+manufacturer=$($SUDO dmidecode |grep -A2 '^System Information' |grep -i "Manufacturer" |sed -e 's/^.*: //')
 if [ ! -n "$manufacturer" ] ; then
     manufacturer="unknown"
 fi
-product=$(sudo dmidecode |grep -A2 '^System Information' |grep -i "Product" |sed -e 's/[^0-9]*//g')
+product=$($SUDO dmidecode |grep -A2 '^System Information' |grep -i "Product" |sed -e 's/[^0-9]*//g')
 if [ ! -n "$product" ] ; then
     product="unknown"
 fi
 # Memory DIMS
-memoryDims=$(sudo dmidecode | grep -c '^[[:space:]]Size: [0-9]* MB')
+memoryDims=$($SUDO dmidecode | grep -c '^[[:space:]]Size: [0-9]* MB')
 if [ ! -n "$memoryDims" ] ; then
     memoryDims="0"
 fi
@@ -39,9 +42,9 @@ fi
 which ethtool >/dev/null 2>&1
 ethExists=$?
 if [ $ethExists -eq 0 ] ; then
-    nicSpeeds=$(/sbin/ip link show | sed '/ lo: /,+1d' | awk '/UP/{sub(":","",$2);print $2}' | xargs -l sudo ethtool | grep -e ^Settings -e Speed |grep "Speed" |sed -e 's/[^0-9]//g')
+    nicSpeeds=$(/sbin/ip link show | sed '/ lo: /,+1d' | awk '/UP/{sub(":","",$2);print $2}' | xargs -l $SUDO ethtool | grep -e ^Settings -e Speed |grep "Speed" |sed -e 's/[^0-9]//g')
 else
-    nicSpeeds=$(/sbin/ip link show | sed '/ lo: /,+1d' | awk '/UP/{sub(":","",$2);print $2}' | xargs -l sudo mii-tool|sed -e 's/^.*negotiated//' -e 's/[^0-9]//g')
+    nicSpeeds=$(/sbin/ip link show | sed '/ lo: /,+1d' | awk '/UP/{sub(":","",$2);print $2}' | xargs -l $SUDO mii-tool|sed -e 's/^.*negotiated//' -e 's/[^0-9]//g')
 fi
 if [ ! -n "$nicSpeeds" ] ; then
     nicSpeeds="1000"
