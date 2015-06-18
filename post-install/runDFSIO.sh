d070a77d25db86624bd69b1e556abc84f06d02db
Vince Gonzalez <vince.gonzalez@gmail.com>
2015-06-18 11:33:54 -0400

    handle fractional disks in YARN
    YARN can have fractions of a disk, e.g., "3.99". Update the grep regex to deal with this, and use awk to round the number.

--- post-install/runDFSIO.sh
+++ post-install/runDFSIO.sh
@@ -29,7 +29,7 @@ if [ $YARN = "true" ] ; then
    HHOME=$(ls -d /opt/mapr/hadoop/hadoop-2*)
    HVER=${HHOME#*/hadoop-}
    TJAR=$(ls $HHOME/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-$HVER-*-tests.jar)
-   tdisks=$(maprcli dashboard info -json | grep total_disks| grep -o '[0-9][0-9]*')
+   tdisks=$(maprcli dashboard info -json | grep total_disks| egrep -o '[0-9]+(\.)([0-9]+)?' | awk '{print int($1+0.5)}')
 
       # Use "mapreduce" properties to force <N> containers per available disk
       # Default is 2 (so map.disk=0.5 and nrFiles is mtasks*2 )
